a184b6cf9c39bc2f00418a1e6cdea5412ff774a4
Fábio Santos <fabiosantosart@gmail.com>
2018-10-24 21:35:38 +0100

    Revert "use flatted instead of its ancestor circular-json"
    This reverts commit 09ca8a73cb34cd4aa1b79d807b90a997751eff8e.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const path = require('path')
 const { spawn, fork } = require('child_process')
 const semver = require('semver')
 const Promise = require('es6-promise')
-const flatted = require('flatted')
+const circularJson = require('circular-json')
 const genericPool = require('./vendor/generic-pool')
 
 const createFile = (contents) => {
@@ -24,14 +24,14 @@ const createFile = (contents) => {
 const multiprocessMap = async (values, fn, { max = os.cpus().length, processStdout = x => x } = {}) => {
   const istanbulVariableMatch = fn.toString().match(/\{(cov_.*?)[[.]/)
   const contents =
-    'var flatted = require("flatted")\n' +
+    'var circularJson = require("circular-json")\n' +
     'var ' + (istanbulVariableMatch ? istanbulVariableMatch[1] : '_cov$$') + ' = {s: [], f: [], b: [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]}\n' +
     'process.on("message", function (msg) {\n' +
-    '  msg = flatted.parse(msg)\n' +
+    '  msg = circularJson.parse(msg)\n' +
     '  require("es6-promise").resolve().then(function () { return (' + fn + ')(msg[0], msg[1], msg[2]) }).then(function (retVal) {\n' +
     '     process.send(flatted.stringify({value: retVal}))\n' +
     '  }, function (error) {\n' +
-    '     process.send(flatted.stringify({error: error}))\n' +
+    '     process.send(circularJson.stringify({error: error}))\n' +
     '  })\n' +
     '})\n' +
     'process.send(null)'
@@ -71,7 +71,7 @@ const multiprocessMap = async (values, fn, { max = os.cpus().length, processStdo
   const ret = await Promise.all(values.map(async (value, index, all) => {
     const cp = await pool.acquire()
     setImmediate(() => {
-      cp.send(flatted.stringify([value, index, all]))
+      cp.send(circularJson.stringify([value, index, all]))
     })
 
     let stdout = ''
@@ -85,7 +85,7 @@ const multiprocessMap = async (values, fn, { max = os.cpus().length, processStdo
 
     cp.stdout.on('data', onData)
 
-    const { value: val, error } = flatted.parse(await new Promise(resolve => {
+    const { value: val, error } = circularJson.parse(await new Promise(resolve => {
       cp.once('message', resolve)
     }))
 
