fe0b79872bb3cdc1b9632803c32db7e6d5b8f095
Fábio Santos <fabiosantosart@gmail.com>
2018-10-26 21:51:19 +0100

    use with-temp-file

--- src/index.js
+++ src/index.js
@@ -9,106 +9,108 @@ const semver = require('semver')
 const Promise = require('es6-promise')
 const circularJson = require('circular-json')
 const genericPool = require('./vendor/generic-pool')
+const withTempFile = require('with-temp-file')
 
-const createFile = (contents) => {
-  const filePath = path.join(__dirname, 'tmp', Math.random() + '.js')
-  fs.writeFileSync(filePath, contents)
-  return {
-    path: filePath,
-    clean: () => {
-      fs.unlinkSync(filePath)
-    }
-  }
-}
+const multiprocessMap = (values, fn, { max = os.cpus().length, processStdout = x => x } = {}) => {
+  return withTempFile(async (ws, filename) => {
+    const istanbulVariableMatch = fn.toString().match(/\{(cov_.*?)[[.]/)
+    const contents =
+      'var circularJson = require("circular-json")\n' +
+      'var userAsyncFunction = require("user-async-function")\n' +
+      'var ' + (istanbulVariableMatch ? istanbulVariableMatch[1] : '_cov$$') + ' = {s: [], f: [], b: [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]}\n' +
+      'process.on("message", function (msg) {\n' +
+      '  msg = circularJson.parse(msg)\n' +
+      '  userAsyncFunction(' + fn + ', msg[0], msg[1], msg[2]).then(function (retVal) {\n' +
+      '     process.send(circularJson.stringify({value: retVal}))\n' +
+      '  }, function (error) {\n' +
+      '     process.send(circularJson.stringify({error: error}))\n' +
+      '  })\n' +
+      '})\n' +
+      'process.send(null)'
+    ws.write(contents)
+    ws.end()
 
-const multiprocessMap = async (values, fn, { max = os.cpus().length, processStdout = x => x } = {}) => {
-  const istanbulVariableMatch = fn.toString().match(/\{(cov_.*?)[[.]/)
-  const contents =
-    'var circularJson = require("circular-json")\n' +
-    'var userAsyncFunction = require("user-async-function")\n' +
-    'var ' + (istanbulVariableMatch ? istanbulVariableMatch[1] : '_cov$$') + ' = {s: [], f: [], b: [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]}\n' +
-    'process.on("message", function (msg) {\n' +
-    '  msg = circularJson.parse(msg)\n' +
-    '  userAsyncFunction(' + fn + ', msg[0], msg[1], msg[2]).then(function (retVal) {\n' +
-    '     process.send(circularJson.stringify({value: retVal}))\n' +
-    '  }, function (error) {\n' +
-    '     process.send(circularJson.stringify({error: error}))\n' +
-    '  })\n' +
-    '})\n' +
-    'process.send(null)'
-  const nodeProcessFile = createFile(contents)
-  const pool = genericPool.createPool({
-    async create () {
-      const cp = semver.satisfies(process.version, '^0.10.0')
-        ? fork(nodeProcessFile.path, [], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'], maxBuffer: 1 })
-        : spawn(process.argv[0], [nodeProcessFile.path], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'], maxBuffer: 1 })
-
-      await new Promise(resolve => {
-        cp.once('message', resolve)
-      })
-
-      return cp
-    },
-    destroy (cp) {
-      cp.disconnect()
-    }
-  }, {
-    max
-  })
-
-  let called = 0
-  const enqueued = []
-  const isLatest = idx => {
-    return idx === called
-  }
-  const enqueue = (idx, fn) => {
-    enqueued[idx] = fn
-
-    while (enqueued[called]) {
-      enqueued[called]()
-      called++
-    }
-  }
-  const ret = await Promise.all(values.map(async (value, index, all) => {
-    const cp = await pool.acquire()
-    setImmediate(() => {
-      cp.send(circularJson.stringify([value, index, all]))
+    await new Promise(resolve => {
+      function check () {
+        if (fs.existsSync(filename)) {
+          return resolve()
+        }
+        setTimeout(check, 300)
+      }
+      check()
     })
 
-    let stdout = ''
-    function onData (data) {
-      if (isLatest(index)) {
-        process.stdout.write(processStdout(data.toString()))
-      } else {
-        stdout += data
+    const pool = genericPool.createPool({
+      async create () {
+        const cp = semver.satisfies(process.version, '^0.10.0')
+          ? fork(filename, [], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'], maxBuffer: 1 })
+          : spawn(process.argv[0], [filename], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'], maxBuffer: 1 })
+
+        await new Promise(resolve => {
+          cp.once('message', resolve)
+        })
+
+        return cp
+      },
+      destroy (cp) {
+        cp.disconnect()
+      }
+    }, {
+      max
+    })
+
+    let called = 0
+    const enqueued = []
+    const isLatest = idx => {
+      return idx === called
+    }
+    const enqueue = (idx, fn) => {
+      enqueued[idx] = fn
+
+      while (enqueued[called]) {
+        enqueued[called]()
+        called++
       }
     }
+    const ret = await Promise.all(values.map(async (value, index, all) => {
+      const cp = await pool.acquire()
+      setImmediate(() => {
+        cp.send(circularJson.stringify([value, index, all]))
+      })
 
-    cp.stdout.on('data', onData)
+      let stdout = ''
+      function onData (data) {
+        if (isLatest(index)) {
+          process.stdout.write(processStdout(data.toString()))
+        } else {
+          stdout += data
+        }
+      }
 
-    const { value: val, error } = circularJson.parse(await new Promise(resolve => {
-      cp.once('message', resolve)
+      cp.stdout.on('data', onData)
+
+      const { value: val, error } = circularJson.parse(await new Promise(resolve => {
+        cp.once('message', resolve)
+      }))
+
+      if (error) throw error
+
+      cp.stdout.removeListener('data', onData)
+
+      enqueue(index, () => {
+        if (stdout) process.stdout.write(processStdout(stdout))
+      })
+
+      pool.release(cp)
+
+      return val
     }))
 
-    if (error) throw error
+    await pool.drain()
+    pool.clear()
 
-    cp.stdout.removeListener('data', onData)
-
-    enqueue(index, () => {
-      if (stdout) process.stdout.write(processStdout(stdout))
-    })
-
-    pool.release(cp)
-
-    return val
-  }))
-
-  await pool.drain()
-  pool.clear()
-
-  setTimeout(() => { nodeProcessFile.clean() }, 1000)
-
-  return ret
+    return ret
+  }, path.join(__dirname, 'tmp', Math.random() + '.js'))
 }
 
 module.exports = multiprocessMap
