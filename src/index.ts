7197c24b7789d178a65e58d55a38b232388fd656
Fábio Santos <fabiosantosart@gmail.com>
2018-11-03 20:36:31 +0000

    use typescript

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,61 @@
 'use strict'
 
-if (!global._babelPolyfill) require('babel-polyfill')
 const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const { spawn, fork } = require('child_process')
 const semver = require('semver')
-const Promise = require('es6-promise')
 const circularJson = require('circular-json')
 const withTempFile = require('with-temp-file')
 const murmur = require('murmurhash')
 const Pool = require('compatible-pool')
 
-const multiprocessMap = (values, fn, { max = os.cpus().length, processStdout = x => x } = {}) => {
-  const istanbulVariableMatch = fn.toString().match(/\{(cov_.*?)[[.]/)
-  const contents =
+type ProcessStdoutFn = (s: string) => string
+
+const wrap = (fn: MapFn) => {
+  const fnStr = fn.toString()
+  const istanbulVariableMatch = fnStr.match(/\{(cov_.*?)[[.]/)
+  return (
     'var circularJson = require("circular-json")\n' +
     'var userAsyncFunction = require("user-async-function")\n' +
     'var ' + (istanbulVariableMatch ? istanbulVariableMatch[1] : '_cov$$') + ' = {s: [], f: [], b: [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]}\n' +
     'process.on("message", function (msg) {\n' +
     '  msg = circularJson.parse(msg)\n' +
-    '  userAsyncFunction(' + fn + ', msg[0], msg[1], msg[2]).then(function (retVal) {\n' +
+    '  userAsyncFunction(' + fnStr + ', msg[0], msg[1], msg[2]).then(function (retVal) {\n' +
     '     process.send(circularJson.stringify({value: retVal}))\n' +
     '  }, function (error) {\n' +
     '     process.send(circularJson.stringify({error: error}))\n' +
     '  })\n' +
     '})\n' +
     'process.send(null)'
+  )
+}
+
+type MapFn = (item: any, index: number, all: any[]) => any
+
+const multiprocessMap = (values: any[], fn: MapFn, {
+  max = os.cpus().length,
+  processStdout = (x: string) => x
+}: {
+  max: number,
+  processStdout: ProcessStdoutFn
+} = { max: os.cpus().length, processStdout: (x: string) => x }) => {
+  const contents = wrap(fn)
   const hash = murmur.v3(contents)
   const filename = path.join(__dirname, 'tmp', hash + '.js')
   const tempFileExists = fs.existsSync(filename)
-  return (tempFileExists ? fn => fn() : withTempFile)(async (ws) => {
+  return (tempFileExists ? (fn: () => any) => fn() : withTempFile)(async (ws: NodeJS.WriteStream) => {
     if (ws) {
       ws.write(contents)
 
-      setImmediate(() => { ws.end() })
-
-      await new Promise(resolve => { ws.on('close', resolve) })
+      ws.end()
 
       await new Promise(resolve => {
         function check () {
           if (fs.existsSync(filename)) {
             return resolve()
           }
-          setTimeout(check, 300)
+          setTimeout(check, 50)
         }
         check()
       })
@@ -56,23 +68,23 @@ const multiprocessMap = (values, fn, { max = os.cpus().length, processStdout = x
           ? fork(filename, [], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'], maxBuffer: 1 })
           : spawn(process.argv[0], [filename], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'], maxBuffer: 1 })
 
-        await new Promise(resolve => {
+        await new Promise<null>(resolve => {
           cp.once('message', resolve)
         })
 
         return cp
       },
-      destroy (cp) {
+      destroy (cp: any) {
         cp.disconnect()
       }
     })
 
     let called = 0
-    const enqueued = []
-    const isLatest = idx => {
+    const enqueued: Array<() => any> = []
+    const isLatest = (idx: number) => {
       return idx === called
     }
-    const enqueue = (idx, fn) => {
+    const enqueue = (idx: number, fn: () => any) => {
       enqueued[idx] = fn
 
       while (enqueued[called]) {
@@ -80,7 +92,7 @@ const multiprocessMap = (values, fn, { max = os.cpus().length, processStdout = x
         called++
       }
     }
-    const ret = await Promise.all(values.map(async (value, index, all) => {
+    const ret = await Promise.all(values.map(async (value: any, index: number, all: any[]): Promise<any> => {
       const cp = await pool.acquire()
       setImmediate(() => {
         cp.send(circularJson.stringify([value, index, all]))
@@ -88,7 +100,7 @@ const multiprocessMap = (values, fn, { max = os.cpus().length, processStdout = x
 
       let stdout = ''
       let isFirstLatestCall = true
-      function onData (data) {
+      function onData (data: any) {
         if (isLatest(index)) {
           if (isFirstLatestCall && stdout) {
             process.stdout.write(processStdout(stdout))
