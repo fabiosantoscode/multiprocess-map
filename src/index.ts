99a5da04a57b6e3814a97866bdc01449ef0b1bfa
Fábio Santos <fabiosantosart@gmail.com>
2018-11-19 14:22:45 +0000

    fix tests

--- src/index.ts
+++ src/index.ts
@@ -6,38 +6,39 @@ type ProcessStdoutFn = (s: string) => string
 
 type MapFn = (item: any, index: number, all: any[]) => any
 
+const defaultMax = os.cpus().length
 const multiprocessMap = async (values: any[], fn: MapFn, {
-  max = os.cpus().length,
+  max = defaultMax,
   processStdout = (x: string) => x
 }: {
   max: number,
   processStdout: ProcessStdoutFn
-} = { max: os.cpus().length, processStdout: (x: string) => x }) => {
+} = { max: defaultMax, processStdout: (x: string) => x }) => {
+  const func = (onMessage, send) => {
+    onMessage(function (msg) {
+      let value
+      try {
+        // tslint:disable:no-eval
+        value = eval(`(${msg[3]})`)(msg[0], msg[1], msg[2])
+      } catch (e) {
+        send({ error: e })
+        return
+      }
+
+      if (value.then) {
+        value.then(function (value) {
+          send({ value })
+        }, function (error) {
+          send({ error: error })
+        })
+      } else {
+        send({ value })
+      }
+    })
+  }
   const pool = new Pool({
     max,
-    create: () => worker.async(
-      function processMap (onMessage, send) {
-        onMessage(function (msg) {
-          let value
-          try {
-            value = (fn)(msg[0], msg[1], msg[2])
-          } catch (e) {
-            send({ error: e })
-            return
-          }
-
-          if (value.then) {
-            value.then(function (ret) {
-              send({ val: ret })
-            }, function (error) {
-              send({ error: error })
-            })
-          } else {
-            send({ val: value })
-          }
-        })
-      }.toString().replace('fn', fn)
-    ),
+    create: () => worker.async(func),
     destroy (w: any) { w.stop() }
   })
 
@@ -52,44 +53,46 @@ const multiprocessMap = async (values: any[], fn: MapFn, {
       called++
     }
   }
-  const ret = await Promise.all(values.map(async (value: any, index: number, all: any): Promise<any> => {
-    const cp = await pool.acquire()
-    setTimeout(() => {
-      cp.send([value, index, all])
-    }, 100)
 
-    let stdout = ''
-    let isFirstLatestCall = true
-    const onStdout = (data: string) => {
-      if (isLatest(index)) {
-        if (isFirstLatestCall && stdout) {
-          process.stdout.write(processStdout(stdout))
+  const ret = await Promise.all(values.map(async (value: any, index: number, all: any) =>
+    new Promise((resolve, reject) => {
+      pool.acquire().then(cp => {
+        cp.send([value, index, all, fn.toString()])
+
+        let stdout = ''
+        let isFirstLatestCall = true
+        const onStdout = (data: string) => {
+          if (isLatest(index)) {
+            if (isFirstLatestCall && stdout) {
+              process.stdout.write(processStdout(stdout))
+            }
+            isFirstLatestCall = false
+            process.stdout.write(processStdout(data))
+          } else {
+            stdout += data
+          }
         }
-        isFirstLatestCall = false
-        process.stdout.write(processStdout(data))
-      } else {
-        stdout += data
-      }
+
+        cp.on('stdout', onStdout)
+
+        return new Promise(resolve => {
+          cp.once('message', resolve)
+        }).then(({ value, error }) => {
+          if (error) { throw error }
+
+          cp.removeListener('stdout', onStdout)
+
+          enqueue(index, () => {
+            if (stdout) { process.stdout.write(processStdout(stdout)) }
+          })
+
+          pool.release(cp)
+
+          resolve(value)
+        })
+      }).catch(reject)
     }
-
-    cp.on('stdout', onStdout)
-
-    const { val, error } = await new Promise(resolve => {
-      cp.once('message', resolve)
-    }) as any
-
-    if (error) { throw error }
-
-    cp.removeListener('stdout', onStdout)
-
-    enqueue(index, () => {
-      if (stdout) { process.stdout.write(processStdout(stdout)) }
-    })
-
-    pool.release(cp)
-
-    return val
-  }))
+  )))
 
   pool.clear()
 
