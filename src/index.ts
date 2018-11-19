b24e75514db4afdf4e5ffb6f01aa3438a538ccef
Fábio Santos <fabiosantosart@gmail.com>
2018-11-19 22:51:20 +0000

    produce es5

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 const os = require('os')
 const Pool = require('compatible-pool')
 const worker = require('parallel-worker')
+const Promise = require('es6-promise')
 
 type ProcessStdoutFn = (s: string) => string
-
 type MapFn = (item: any, index: number, all: any[]) => any
 
 const defaultMax = os.cpus().length
-const multiprocessMap = async (values: any[], fn: MapFn, {
+const multiprocessMap = (values: any[], fn: MapFn, {
   max = defaultMax,
   processStdout = (x: string) => x
 }: {
   max: number,
   processStdout: ProcessStdoutFn
-} = { max: defaultMax, processStdout: (x: string) => x }) => {
+} = { max: defaultMax, processStdout: (x: string) => x }): Promise<any[]> => {
   const func = `(onMessage, send) => {
     onMessage(function (msg) {
       let value
@@ -54,49 +54,50 @@ const multiprocessMap = async (values: any[], fn: MapFn, {
     }
   }
 
-  const ret = await Promise.all(values.map(async (value: any, index: number, all: any) =>
-    new Promise((resolve, reject) => {
-      pool.acquire().then(cp => {
-        cp.send([value, index, all])
+  return Promise.all(values.map(
+    (value: any, index: number, all: any) =>
+      new Promise((resolve, reject) => {
+        pool.acquire().then(cp => {
+          cp.send([value, index, all])
 
-        let stdout = ''
-        let isFirstLatestCall = true
-        const onStdout = (data: string) => {
-          if (isLatest(index)) {
-            if (isFirstLatestCall && stdout) {
-              process.stdout.write(processStdout(stdout))
+          let stdout = ''
+          let isFirstLatestCall = true
+          const onStdout = (data: string) => {
+            if (isLatest(index)) {
+              if (isFirstLatestCall && stdout) {
+                process.stdout.write(processStdout(stdout))
+              }
+              isFirstLatestCall = false
+              process.stdout.write(processStdout(data))
+            } else {
+              stdout += data
             }
-            isFirstLatestCall = false
-            process.stdout.write(processStdout(data))
-          } else {
-            stdout += data
           }
-        }
 
-        cp.on('stdout', onStdout)
+          cp.on('stdout', onStdout)
 
-        return new Promise(resolve => {
-          cp.once('message', resolve)
-        }).then(({ value, error }) => {
-          if (error) { throw error }
+          return new Promise(resolve => {
+            cp.once('message', resolve)
+          }).then(({ value, error }) => {
+            if (error) { throw error }
 
-          cp.removeListener('stdout', onStdout)
+            cp.removeListener('stdout', onStdout)
 
-          enqueue(index, () => {
-            if (stdout) { process.stdout.write(processStdout(stdout)) }
-          })
+            enqueue(index, () => {
+              if (stdout) { process.stdout.write(processStdout(stdout)) }
+            })
 
-          pool.release(cp)
+            pool.release(cp)
 
-          resolve(value)
+            resolve(value)
+          }).catch(reject)
         }).catch(reject)
-      }).catch(reject)
-    }
-  )))
-
-  pool.clear()
-
-  return ret
+      }
+    )))
+  .then(ret => {
+    pool.clear()
+    return ret
+  })
 }
 
 module.exports = multiprocessMap
